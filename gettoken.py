8b05fa8e4d9a216a41f5e51eb51135961c28706f
charliecaplin <91089814+charliecaplin@users.noreply.github.com>
2021-12-26 11:32:51 +0700

    update
    update

--- gettoken.py
+++ gettoken.py
@@ -1,11 +1,12 @@
 from flask import Flask, Response, jsonify, abort, make_response,  render_template, redirect, request
+#from faster_than_requests import requests
 import requests
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import json, requests, traceback, os, lxml, re, urllib, urllib3, urllib.parse, argparse, pytz, urllib.request, time, httpx
 
 
-app = Flask(__name__, static_url_path='/qrimage")
+app = Flask(__name__, static_url_path="/qrimage")
 
 @app.route('/')
 def home():
@@ -78,7 +79,7 @@ def one(q):
             file = open("/qrimage/{}.png".format(crypto), "wb")
             file.write(r.content)
             file.close()
-            imagenya = "/qrimage/{}.png".format(crypto)
+            imagenya = "/qrimages/{}.png".format(crypto)
             baseurl = request.base_url.split("/crlgs/")[0]
             resultdict["qrimage"] = baseurl + imagenya
             return make_response(jsonify(resultdict))
@@ -94,9 +95,15 @@ def one(q):
             qrlink = qr["result"]["qrlink"] #your qrlink
             resultdict["qrlink"] = qrlink
             qrcode = qr["result"]["qrcode"] #your qrcode
-            resultdict["qrcode"] = qrcode
             crypto = qr["result"]["session"] #your qrcode
             resultdict["crypto"] = crypto
+            r = requests.get(qrcode)
+            file = open("/qrimage/{}.png".format(crypto), "wb")
+            file.write(r.content)
+            file.close()
+            imagenya = "/qrimage/{}.png".format(crypto)
+            baseurl = request.base_url.split("/crlgs/")[0]
+            resultdict["qrimage"] = baseurl + imagenya
             return make_response(jsonify(resultdict))
         except Exception as error:
             print (error)
@@ -104,7 +111,42 @@ def one(q):
 
 @app.route("/crqr/<q>")
 def two(q):
+    class BEAPI():
+        def __init__(self):
+            self.host = "https://beta.beapi.me"
+            self.version = "1.3"
+            self.http = httpx.Client(http2=True,timeout=120)
+        def lineAppname(self):
+            resp = self.http.get(self.host+"/lineappname").json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def lineAppnameRandom(self, osname):
+            #["android","ios","androidlite","chromeos","desktopmac","desktopwin","iosipad"]
+            params = {"osname": osname}
+            resp = self.http.get(self.host+"/lineappname_random",params=params).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def linePrimary2Secondary(self, appName, authToken):
+            params = {"appname": appName, "authtoken": authToken}
+            resp = self.http.get(self.host+"/lineprimary2secondary",params=params).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def lineGetQr(self, appName, cert=None):
+            params = {"appname": appName}
+            if cert: params["cert"] = cert
+            resp = self.http.get(self.host+"/lineqr",params=params).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def lineGetQrPincode(self, session):
+            resp = self.http.get(self.host+"/lineqr/pincode/"+session).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def lineGetQrAuth(self, session):
+            resp = self.http.get(self.host+"/lineqr/auth/"+session).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
     try:
+        api = BEAPI()
         pincode = api.lineGetQrPincode(q)
         sys = "rm /qrimage/{}.png"
         os.system(sys)
@@ -115,7 +157,42 @@ def two(q):
 
 @app.route("/crlgs2/<q>")
 def three(q):
+    class BEAPI():
+        def __init__(self):
+            self.host = "https://beta.beapi.me"
+            self.version = "1.3"
+            self.http = httpx.Client(http2=True,timeout=120)
+        def lineAppname(self):
+            resp = self.http.get(self.host+"/lineappname").json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def lineAppnameRandom(self, osname):
+            #["android","ios","androidlite","chromeos","desktopmac","desktopwin","iosipad"]
+            params = {"osname": osname}
+            resp = self.http.get(self.host+"/lineappname_random",params=params).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def linePrimary2Secondary(self, appName, authToken):
+            params = {"appname": appName, "authtoken": authToken}
+            resp = self.http.get(self.host+"/lineprimary2secondary",params=params).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def lineGetQr(self, appName, cert=None):
+            params = {"appname": appName}
+            if cert: params["cert"] = cert
+            resp = self.http.get(self.host+"/lineqr",params=params).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def lineGetQrPincode(self, session):
+            resp = self.http.get(self.host+"/lineqr/pincode/"+session).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
+        def lineGetQrAuth(self, session):
+            resp = self.http.get(self.host+"/lineqr/auth/"+session).json()
+            if resp["status"] != 200: raise Exception (resp["reason"])
+            return resp
     try:
+        api = BEAPI()
         resultnya = {}
         auth = api.lineGetQrAuth(q)
         d = auth["result"]["accessToken"]
@@ -126,8 +203,8 @@ def three(q):
         print (error)
         #print ("error, contact the creator")
 
-@app.route("/qrimage/<q>")
-def three(q):
+@app.route("/getqrimage/<q>")
+def four(q):
     try:
         return app.send_static_file(q)
     except Exception as error:
